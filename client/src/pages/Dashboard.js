824ed8610f02b4e345cac191074f05986bdeb6f5
Elihu Chitrit <eli@qwamo.com>
2023-05-29 14:00:42 +0300

    29052023

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 import { Button, Container, Row, Col, Form, Card, Table, Modal } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from '../components/Header';
@@ -9,6 +9,7 @@ import moment from 'moment';
 import RowEdit from "../components/RowEdit";
 import { NavLink } from 'react-router-dom';
 import { VscChevronRight } from "react-icons/vsc";
+import axios from 'axios';
 
 
 const Dashboard = props => {
@@ -20,19 +21,20 @@ const Dashboard = props => {
     const [gallery, setgallery] = useState([]);
     const [reviewsIsEditable, setReviewsIsEditable] = useState(false);
 
-
-
-    //const [title, setTitle] = useState("");
-    //const [review, setReview] = useState("");
     const [counter, setCounter] = useState(0);
-
     const [rowItem, setRowItem] = useState({
         title: '',
         review: '',
         tid: 0
     });
 
+    const [imageRow, setImageRow] = useState({
+        imageSource: '',
+        imageDesc: ''
+    })
+
     const {title,review,tid} = rowItem;
+    const {imageSource, imageDesc} = imageRow;
 
     //FUNCTIONS
     const addReviewToList = () => {
@@ -49,6 +51,23 @@ const Dashboard = props => {
             toast.error("All inputs are required");
         }
     }
+
+
+    const addImageToGallery = () => {
+        if(imageSource !== "" && imageDesc !== ""){
+            setCounter(counter + 1);
+            let row = {
+                tid: counter,
+                imageSource: imageSource,
+                imageDesc: imageDesc
+            };
+            setgallery(gallery => [...gallery, row]);
+        } else {
+            toast.error("All inputs are required");
+        }
+    }
+
+
     const deleteRowFromList = (tid) => {
         setreviews((state) => state.filter((item) => item.tid !== tid));
     }
@@ -87,6 +106,9 @@ const Dashboard = props => {
         setreviews(erow);
     }
 
+    const preset_key = 'vq1ypl7h';
+    const cloud_name = 'da9u18hd0';
+
     const onItemChange = (e) => {
         setRowItem((prev) => ({
             ...prev,
@@ -94,8 +116,74 @@ const Dashboard = props => {
         }))
     }
 
+    const onImageChange = async(e) => {
+
+        let value = '';
+
+        if(e.target.name === 'imageSource'){
+
+            const file = e.target.files[0];
+            const formData = new FormData();
+            formData.append('file', file);
+            formData.append('upload_preset', preset_key);
+            axios.post(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, formData)
+            .then(async results => {
+                value = await results.data.secure_url;
+                setImageRow((prev) => ({
+                    ...prev,
+                    [e.target.name]: value
+                }))
+            })
+            .catch(error => {
+                toast.error(error.message)
+            })
+        } else {
+            value = e.target.value
+            setImageRow((prev) => ({
+                ...prev,
+                [e.target.name]: value
+            }))
+        }
+    }
+
+    const baseURL = "http://localhost:3001/api";
+
+
+    const [search, setSearch] = useState(null);
+
+    useEffect(() => {
+        let arr = gallery;
+
+        arr.filter(x => x.imageDesc === search);
+
+        setgallery(arr)
+
+    },[search])
+
+
+    console.log(imageRow);
+
+    const adddevice = () => {
+        const device = {
+            deviceName: deviceName,
+            price: price,
+            reviews: reviews,
+            gallery: gallery
+        }
+
+        axios.post(baseURL + '/account/createDevice',{device})
+        .then(results => {
+            console.log(results)
+        })
+        .catch(error => {
+            toast.error(error.response.data.message);
+        })
+    }
+
+
 
     return(
+
 
         <>
 
@@ -104,6 +192,28 @@ const Dashboard = props => {
             <Header />
             <ToastContainer />
             <Container style={{marginTop:50}}>
+{/* 
+                <Row>
+                    <Col xl={12}>
+                        <Form.Control value={search} onChange={(e) => {setSearch(e.target.value)}} style={{width:250}} />
+                    </Col>
+                </Row> */}
+
+
+                <Row>
+                    <Col xl={5}>
+                        <Form.Control value={deviceName} onChange={(e) => {setdeviceName(e.target.value)}} style={{width:250}} />
+                    </Col>
+                    <Col xl={5}>
+                        <Form.Control value={price} onChange={(e) => {setprice(e.target.value)}} style={{width:250}} />
+                    </Col>
+                    <Col xl={2}>
+                        <Button onClick={adddevice}>ADD DEVICE</Button>
+                    </Col>
+                </Row>
+
+
+<br/><br/>
                 <Row>
                     <Col xl={5}>
                         <Form.Control value={title} name="title" onChange={(e) => {onItemChange(e)}} type="text" placeholder="Type your title..." />
@@ -175,8 +285,66 @@ const Dashboard = props => {
                     </Col>
                 </Row>
 
+                <Row>
+                    <Col xl={5}>
+                        <Form.Control name="imageSource" type="file" onChange={(e) => {onImageChange(e)}} />
+                    </Col>
+                    <Col xl={5}>
+                        <Form.Control value={imageDesc} name="imageDesc" onChange={(e) => {onImageChange(e)}} type="text" placeholder="Type your description..." />
+                    </Col>
+                    <Col xl={2}>
+                        <Button onClick={addImageToGallery} variant="outline-success"><FcPlus size={30} /></Button>
+                        <Button style={{marginLeft:10}} onClick={editList} variant="outline-info"><FcEditImage size={30} /></Button>
+                    </Col>
+                </Row>
+
+
+                <Row>
+                    <Col xl={12}>
+                        <Table>
+                            <thead>
+                                <tr>
+                                    <th>Image</th>
+                                    <th>Desc</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    gallery.length > 0 && (<>
+                                    
+                                        {
+                                            gallery.map((item) => (
+                                                <>
+                                                {
+
+                                                        <tr>
+                                                            <td>
+                                                                <img src={item.imageSource} style={{width:200}} />
+                                                            </td>
+                                                            <td>{item.imageDesc}</td>
+                                                            <td></td>
+                                                        </tr>
+         
+                                                }
+                                                </>
+
+                                                
+                                            ))
+                                        }
+                                    </>)
+                                }
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+
+
+
+
 
             </Container>
+
 
             
 
